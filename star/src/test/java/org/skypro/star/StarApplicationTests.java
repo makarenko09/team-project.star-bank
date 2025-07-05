5fb78018483e8765f48dcff8970772075564df09
makarenko009 <konmakarenko@outlook.com>
2025-07-05 23:21:07 +0300

    initial commit

---
+++ star/src/test/java/org/skypro/star/StarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
