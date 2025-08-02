22cbb9024da32bec6f26ce4d9c893eece555aced
makarenko09 <163112091+makarenko09@users.noreply.github.com>
2025-08-02 15:06:46 +0300

    feat(DTO): select DTO type class to record

--- src/main/java/org/skypro/star/model/Recommendation.java
+++ src/main/java/org/skypro/star/model/Recommendation.java
@@ -5,10 +5,6 @@ import lombok.Data;
 
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-public class Recommendation {
-    private final String name;
-    private final UUID id;
-    private final String text;
+
+public record Recommendation(String name, UUID id, String text) {
 }
